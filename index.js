f981652ed09c9648eb3e59c48cb93f3426affa06
Nils Matic <173457306+nirusu10@users.noreply.github.com>
2025-05-06 14:53:08 +0200

    add function 'checkForWin' to check for winnign condition

--- index.js
+++ index.js
@@ -28,9 +28,55 @@ const gameController = (function () {
 
   let currentPlayer = player1
 
+  const _checkRows = function () {
+    return (
+      (Gameboard.getField(0) &&
+        Gameboard.getField(0) === Gameboard.getField(1) &&
+        Gameboard.getField(1) === Gameboard.getField(2)) ||
+      (Gameboard.getField(3) &&
+        Gameboard.getField(3) === Gameboard.getField(4) &&
+        Gameboard.getField(4) === Gameboard.getField(5)) ||
+      (Gameboard.getField(6) &&
+        Gameboard.getField(6) === Gameboard.getField(7) &&
+        Gameboard.getField(7) === Gameboard.getField(8))
+    )
+  }
+
+  const _checkCols = function () {
+    return (
+      (Gameboard.getField(0) &&
+        Gameboard.getField(0) === Gameboard.getField(3) &&
+        Gameboard.getField(3) === Gameboard.getField(6)) ||
+      (Gameboard.getField(1) &&
+        Gameboard.getField(1) === Gameboard.getField(4) &&
+        Gameboard.getField(4) === Gameboard.getField(7)) ||
+      (Gameboard.getField(2) &&
+        Gameboard.getField(2) === Gameboard.getField(5) &&
+        Gameboard.getField(5) === Gameboard.getField(8))
+    )
+  }
+
+  const _checkDiag = function () {
+    return (
+      (Gameboard.getField(0) &&
+        Gameboard.getField(0) === Gameboard.getField(4) &&
+        Gameboard.getField(4) === Gameboard.getField(8)) ||
+      (Gameboard.getField(2) &&
+        Gameboard.getField(2) === Gameboard.getField(4) &&
+        Gameboard.getField(4) === Gameboard.getField(6))
+    )
+  }
+
+  const checkForWin = function () {
+    return _checkCols() || _checkRows() || _checkDiag()
+  }
+
   const makeMove = function (field) {
     Gameboard.setField(field, currentPlayer.getSign())
     currentPlayer = currentPlayer === player1 ? player2 : player1
+    if (checkForWin()) {
+      console.log('WINNER WINNER CHICKEN DINNER')
+    }
   }
 
   return { makeMove }
